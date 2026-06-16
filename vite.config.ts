39a569cd65e65d1a8050bf60e4510701dd4ec952
eociV <h4vinh@gmail.com>
2026-06-16 21:58:16 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      ignored: [
+        '**/tsconfig.json',
+        '**/tsconfig.app.json',
+        '**/tsconfig.node.json',
+        '**/vite.config.ts',
+      ],
+    },
+  },
 })
